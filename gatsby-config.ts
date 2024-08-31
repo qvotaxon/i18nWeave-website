f431c4904ee2be90c9a8459214ee25c9cf35cd74
Jordy Vervloed <2746261+qvotaxon@users.noreply.github.com>
2024-08-31 11:25:47 +0200

    chore: set up initial project configuration
    Add package.json with dependencies and scripts for Gatsby, React, TypeScript, and TailwindCSS. Configure TailwindCSS in tailwind.config.
    Setup tsconfig.json with necessary TypeScript compiler options for project setup.

---
+++ gatsby-config.ts
@@ -0,0 +1,47 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `i18nWeave - Developer's i18n Companion`,
+    siteUrl: `https://i18nweave.com`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'G-GY3TDG8CD7', // Google Analytics / GA
+        ],
+        // pluginConfig: {
+        //   head: true,
+        //   anonymize_ip: true,
+        // },
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+};
+
+export default config;

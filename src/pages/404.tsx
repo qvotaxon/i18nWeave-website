63ab7776cd5d510d86789bd46da60b9c9c361989
Jordy Vervloed <2746261+qvotaxon@users.noreply.github.com>
2024-09-07 20:10:52 +0200

    feat: add new features page and translation keys
    Add FeaturesPage component to display key features of i18nWeave, including automatic key extraction and automatic translation features. Update translation keys in both English and Dutch locales to reflect these new features. Adjust 404 page styling for better responsiveness on smaller screens.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,28 +4,17 @@ import { StaticImage } from 'gatsby-plugin-image';
 import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import LanguageSelector from '../libs/ui/ui-language-selector/src/lib/language-selector';
 import {Layout} from '@i18n-weave/ui/ui-layout';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   const { t } = useTranslation();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
 
   return (
     <Layout>
-      <section className="h-screen flex flex-col items-center bg-variant-1 snap-start scroll-mt-16 pt-8 text-center">
+      <section className="h-screen flex flex-col items-center bg-variant-1 snap-start scroll-mt-16 pt-8 text-center [@media_(max-height:666px)]:h-fit">
         <h1 className="text-4xl">{t('404.pageNotFound.title')}</h1>
         <p className='text-2xl pt-6'>{t('404.pageNotFound.errorMessage')}</p>
         <p className='text-6xl pt-6'>¯\_(ツ)_/¯</p>
-        <StaticImage
-          src="../images/404.png"
-          alt="404 Error"
-          placeholder="blurred"
-          layout="fixed"
-          width={300}
-          height={300}
-        />
       </section>
     </Layout>
   );

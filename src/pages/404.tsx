c0158c5438b29b40d59f8db15fc04f4d9451fd08
Jordy Vervloed <2746261+qvotaxon@users.noreply.github.com>
2024-09-07 18:58:06 +0200

    chore: update dependencies to resolve conflicts
    Update tslib to version 2.4.1 to match other dependencies. Remove @eslint/js as it is no longer needed. Adjust eslint dependencies to version 7.32.0 for compatibility with other packages.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,52 +1,62 @@
 import * as React from 'react';
-import { Link, HeadFC, PageProps } from 'gatsby';
+import { PageProps, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
+import LanguageSelector from '../libs/ui/ui-language-selector/src/lib/language-selector';
+import {Layout} from '@i18n-weave/ui/ui-layout';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   const { t } = useTranslation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>{t('notfound.title')}</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <section className="h-screen flex flex-col items-center bg-variant-1 snap-start scroll-mt-16 pt-8 text-center">
+        <h1 className="text-4xl">{t('404.pageNotFound.title')}</h1>
+        <p className='text-2xl pt-6'>{t('404.pageNotFound.errorMessage')}</p>
+        <p className='text-6xl pt-6'>¯\_(ツ)_/¯</p>
+        <StaticImage
+          src="../images/404.png"
+          alt="404 Error"
+          placeholder="blurred"
+          layout="fixed"
+          width={300}
+          height={300}
+        />
+      </section>
+    </Layout>
   );
 };
 
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
+
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head = () => (
+  <>
+    <title>i18nWeave - Page Not Found</title>
+    <meta
+      name="description"
+      content="i18nWeave helps developers efficiently handle translations in their projects. Increase productivity and ensure consistency across multiple languages."
+    />
+    <meta
+      name="keywords"
+      content="i18n, react, next.js, angular, i18n-next, deepl, internationalization, VSCode extension, translations, developer tools"
+    />
+  </>
+);

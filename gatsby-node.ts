3a09eea5d80086e3dd84ac707bf4bf8df75ff359
Jordy Vervloed <2746261+qvotaxon@users.noreply.github.com>
2024-09-19 19:54:43 +0200

    refactor: [skip ci] update function args for better type safety
    Add type annotations to actions and getConfig parameters in onCreateWebpackConfig function to enhance type safety and improve code readability.

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,6 +1,12 @@
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
-export const onCreateWebpackConfig = async ({ actions, getConfig }) => {
+export const onCreateWebpackConfig = async ({
+  actions,
+  getConfig,
+}: {
+  actions: any;
+  getConfig: any;
+}) => {
   const config = getConfig();
 
   config.resolve.plugins = config.resolve.plugins || [];
